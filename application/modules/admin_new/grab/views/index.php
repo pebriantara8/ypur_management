68e4ebab79510446acd6fb4a32045e8c23337cd2
pebriantara <pebriantara8@gmail.com>
2019-04-27 11:28:47 +0700

    a

--- application/modules/admin_new/grab/views/index.php
+++ application/modules/admin_new/grab/views/index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="baseUrl" content="<?=base_url()?>"/>
   <link rel="icon" type="image/png" href="<?=base_url()?>assets/sp/images/logo.svg">
-  <title>Admin ESoC</title>
+  <title>YPur Management</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
